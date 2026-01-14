6d8a4b2ab4f1d12001fc33cf7657d055f6941420
Luke Bartup <luke.bartup@students.plymouth.ac.uk>
2026-01-14 15:59:15 +0000

    Git didnt sync, Prototype 2
    git didnt sync so since last commit added: Java script functionality for creating and saving new notes, Storing saved notes to localstorage, displaying notes in noteslist and CSS styling for noteslist.

---
+++ script.js
@@ -0,0 +1,102 @@
+let notes = [];
+
+const newnoteBtn = document.getElementById('newnoteBtn');
+const noteEditor = document.getElementById('noteEditor');
+const welcome = document.getElementById('welcome');
+const noteTitle = document.getElementById('noteTitle');
+const noteContent = document.getElementById('noteContent');
+const saveBtn = document.getElementById('saveBtn');
+
+
+function createNewNote() {
+    console.log("creating new note");
+
+    //hide welcome message, show note editor
+    welcome.style.display = 'none';
+    noteEditor.style.display = 'block';
+
+    //clear
+    noteTitle.value = '';
+    noteContent.value = '';
+
+    noteTitle.focus();
+}
+function savetoLocalStorage() {
+    localStorage.setItem('myNotes', JSON.stringify(notes));
+}
+function loadNotes() {
+    const savedNotes = localStorage.getItem('myNotes');
+
+    if (savedNotes) {
+        notes = JSON.parse(savedNotes);
+    } else {
+        notes = [
+            {
+                id: 1,
+                title: "welcome to SecureNote",
+                content: "this is an example note",
+                date: "Today"
+            }
+        ];
+        savetoLocalStorage();
+    }
+}
+
+function saveNote() {
+    const title = noteTitle.value;
+    const content = noteContent.value;
+
+    if (!title.trim()) {
+        alert("Please enter a title");
+        return;
+    }
+
+    //creating new note object
+    const newNote = {
+        id: Date.now(),
+        title: title,
+        content: content,
+        date: new Date().toLocaleDateString()
+    };
+
+    notes.push(newNote);
+
+    savetoLocalStorage();
+
+    console.log("savig note:", title);
+    alert("Note Saved: " + title);
+
+    //display welcome message agian
+    welcome.style.display = 'block';
+    noteEditor.style.display = 'none';
+
+    updateNotesList();
+}
+
+function updateNotesList() {
+    const notesList = document.getElementById('notesList');
+    notesList.innerHTML = '';
+
+    notes.forEach(note => {
+        const noteElement = document.createElement('div');
+        noteElement.className = 'note-item';
+        noteElement.innerHTML = `<h3>${note.title}</h3>  <p>${note.content.substring(0,50)}....</p> <small>${note.date}</small>`;
+
+notesList.appendChild(noteElement);
+    })
+}
+
+//initializing the app
+function init() {
+    console.log("NoteApp intitialized");
+
+    loadNotes();
+
+    newnoteBtn.addEventListener('click', createNewNote);
+        saveBtn.addEventListener('click', saveNote);
+
+        updateNotesList();
+}
+
+//run inittialisation when page is loaded
+document.addEventListener('DOMContentLoaded', init);
